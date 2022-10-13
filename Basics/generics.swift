a34b959b6002963aa574ef188e97f30fc53bb08c
Ranga Reddy Nukala <itsmerr17@gmail.com>
2022-10-14 00:05:36 +0530

    add generic struct

--- Basics/generics.swift
+++ Basics/generics.swift
@@ -4,8 +4,26 @@ Generics
 --------
 */
 
+import Foundation
+
 var array = ["one", "two", "three"]
 array.append("four")
-array.append(5)
+
+// compile time error: since array has string data
+// array.append(5)
 
 print(array)
+
+
+// custom generic content
+
+
+struct Container<Value> {
+    var value: Value
+    var date: Date
+}
+
+let stringContainer = Container(value: "Test", date: Date())
+let intContainer = Container(value: 1, date: Date())
+
+print(stringContainer, intContainer)
